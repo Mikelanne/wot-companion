f45a94ab9794bb200f02d40aa48cf1a4bd0f66c8
Mikelanne <mikelanne.wood@gmail.com>
2021-08-14 19:56:13 -0400

    adding notes

--- src/components/home.js
+++ src/components/home.js
@@ -3,9 +3,14 @@ const Home = () => {
     return (
         <div>
             <h1>Welcome to your Wheel of Time Companion</h1>
+            <h3>This is your spoiler free companion to Robert Jordan's Wheel of Time series.</h3>
+
+        {/* form goes here for log in */}
         </div>
     )
 
 }
 
 export default Home
+
+{/*Home screen will have a Welcome, a little info, a form to log in and a link directing you to the sign up route*/}

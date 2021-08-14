0cd035f0ca94a5de518d8a8e6f7c16a271b72dd7
Mikelanne <mikelanne.wood@gmail.com>
2021-08-14 19:50:20 -0400

    adding router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import './App.css'
 import CharactersContainer from './containers/charactersContainer'
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
+import Home from "./components/home"
 
 function App() {
   return (
     <div>
-      <CharactersContainer />
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </Router>
+      {/* <CharactersContainer /> */}
     </div>
   );
 }

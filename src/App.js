36b2b4515703bf242169c693405e931a5ef02396
Mikelanne <mikelanne.wood@gmail.com>
2021-08-25 10:20:10 -0400

    sticking with plain React for the user functionality

--- src/App.js
+++ src/App.js
@@ -2,20 +2,36 @@ import './App.css'
 import CharactersContainer from './containers/charactersContainer'
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 import Home from "./components/home"
+import { Component } from 'react';
 
-function App() {
-  return (
-    <div>
-      <Router>
-        <Switch>
-          <Route exact path="/">
-            <Home />
-          </Route>
-        </Switch>
-      </Router>
-      {/* <CharactersContainer /> */}
-    </div>
-  );
+export default class App extends Component {
+
+  constructor() {
+    super();
+
+    this.state = {
+      loggedInStatus: "NOT_LOGGED_IN",
+      user: {}
+    }
+  }
+
+  render() {
+    return (
+        <div>
+          <Router>
+            <Switch>
+              <Route 
+                exact path={"/"}
+                render={props => (
+                  <Home {... props} loggedInStatus={this.state.loggedInStatus} />
+                )}>
+                {/* <Home /> */}
+              </Route>
+            </Switch>
+          </Router>
+          {/* <CharactersContainer /> */}
+        </div>
+      )
+    }
+
 }
-
-export default App;
--- src/components/home.js
+++ src/components/home.js
@@ -1,18 +1,24 @@
+import { Component } from "react"
 import Registration from "./auth/Registration"
 
-const Home = () => {
+export default class Home extends Component {
 
-    return (
-        <div>
-            <h1>Welcome to your Wheel of Time Companion</h1>
-            <h3>This is your spoiler free companion to Robert Jordan's Wheel of Time series.</h3>
-            <Registration />
-        {/* form goes here for log in */}
-        </div>
-    )
+    constructor(props) {
+        super(props);
 
+
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>Welcome to your Wheel of Time Companion</h1>
+                <h3>This is your spoiler free companion to Robert Jordan's Wheel of Time series.</h3>
+                <h1>Status: {this.props.loggedInStatus}</h1>
+                <Registration /> 
+            </div>
+        )
+    }
 }
-
-export default Home
 
 {/*Home screen will have a Welcome, a little info, a form to log in and a link directing you to the sign up route*/}

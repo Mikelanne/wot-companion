f3d50b9b7d5cfef339cb4938c686988d9252a445
Mikelanne <mikelanne.wood@gmail.com>
2021-10-08 17:46:23 -0400

    log in and sign up complete

--- src/components/home.js
+++ src/components/home.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import {Link} from 'react-router-dom'
 
-const Home = () => {
+const Home = (props) => {
     return (
         <div>
+            <h1>Home</h1>
             <Link to='/login'>Log In</Link>
             <br></br>
             <Link to='/signup'>Sign Up</Link>

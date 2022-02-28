a38f0d5c9dd7ac8530bd8a06789cf61c197806a9
Mikelanne <mikelanne.wood@gmail.com>
2022-02-28 17:30:12 -0500

    sending a get request to recieve characters

--- src/components/dashboard.js
+++ src/components/dashboard.js
@@ -1,13 +1,38 @@
+import axios from "axios";
 import React from "react";
+import { Component } from "react";
 
-const Dashboard = (props) => {
+export default class Dashboard extends Component  {
 
-    return (
+    constructor(props) {
+        super(props);
+
+    }
+
+    getCharacters() {
+        axios.get('http://localhost:3001/characters')
+        .then(response => {
+           this.handleGetCharacters(response.data)
+        }).catch(error => {
+            console.log ("character error", error)
+        })
+    }
+
+    handleGetCharacters(data) {
+        const characterArray = data.data
+        const character = characterArray.map(guy => (guy.attributes.name))
+        console.log(character)
+        }
+
+    componentDidMount(){
+        this.getCharacters()
+    }
+
+    render() {return (
         <div>
-            <h1>Welcome Back To The Adventure, {props.user.username}!</h1>
-            <h1>Status: {props.loggedInStatus}</h1>
+            <h1>Welcome Back To The Adventure, {this.props.user.username}!</h1>
+            <h1>Status: {this.props.loggedInStatus}</h1>
         </div>
-    )
+    )}
 }
 
-export default Dashboard
